5ac00f70401a840fce9525214c116b6ea139274a
Yogin Patel <ypatel@sprinklr.com>
2017-04-06 01:30:07 +0530

    Add icon, Add .gitignore, Add localstorage & index.html for BrowserWindow

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
-const {app, Menu, Tray} = require('electron')
+const {app, BrowserWindow, Menu, Tray} = require('electron')
+const path = require('path')
+const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let tray = null
+let win = null;
 
 function createTray() {
+  win = new BrowserWindow({show: false});
+  tray = new Tray(path.join(__dirname, 'assets/icons/copy-icon.png'));
 
-  tray = new Tray('/Users/yoginpatel/Downloads/icon.png')
+  tray.setToolTip('Open clipboard logs');
 
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'},
-    {label: 'Item3', type: 'radio', checked: true},
-    {label: 'Item4', type: 'radio'}
-  ])
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
+  // When the tray icon is clicked, log to our console
+  tray.on('click', function handleClicked () {
+    win.show();
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
 
+    console.log('Tray clicked');
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createTray)
+app.on('ready', createTray);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
